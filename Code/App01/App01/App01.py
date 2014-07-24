527e1b791c428abb1d7be46bb663a6d40c59dbaf
fahad623 <fahad2191@gmail.com>
2014-07-23 22:55:21 -0500

    Modify

--- Code/App01/App01/App01.py
+++ Code/App01/App01/App01.py
@@ -23,11 +23,37 @@ def get_poll_xml(id):
     r = requests.get(url)
     return r.text
 
-#root = ET.fromstring(get_poll_xml(1044))
-#recurse_node(0, root)
-dom = web.Element(get_poll_xml(1044))
-result = {}
-for graph in dom.by_tag('graph'):
-    title = _strip(graph.attributes['title'])
-    result[title] = graph.attributes['color']
-print result
+#xmlText  = get_poll_xml(1044)
+
+xmlText = '''
+    <chart>
+    <series>
+    <value xid="0">1/27/2009</value>
+    <value xid="1">1/28/2009</value>
+    </series>
+    <graphs>
+    <graph gid="1" color="#000000" balloon_color="#000000" title="Approve">
+    <value xid="0">63.3</value>
+    <value xid="1">63.3</value>
+    </graph>
+    <graph gid="2" color="#FF0000" balloon_color="#FF0000" title="Disapprove">
+    <value xid="0">20.0</value>
+    <value xid="1">20.0</value>
+    </graph>
+    </graphs>
+    </chart>
+    '''
+
+dom = web.Element(xmlText)
+
+
+
+listDates = []
+series = dom.by_tag('series')
+values = series[0].by_tag('value')
+for value in values:
+    listDates.append(value.content)
+
+frame = pd.DataFrame({'date': pd.to_datetime(listDates)})
+
+print frame
