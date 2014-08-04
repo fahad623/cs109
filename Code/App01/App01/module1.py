c0f3f3803d2f4274e09d6d8693e7dfba6edb9467
fahad623 <fahad2191@gmail.com>
2014-08-03 22:59:16 -0500

    another commit

--- Code/App01/App01/module1.py
+++ Code/App01/App01/module1.py
@@ -1,3 +1,21 @@
 import numpy as np
+import pandas as pd
 
-print np.random.randint(0, 10, 5)
+from collections import defaultdict
+import json
+
+predictwise = pd.read_csv('predictwise.csv').set_index('States')
+predictwise.head()
+
+print np.random.uniform(0, 1, 3)
+
+mask =  predictwise.Obama > np.random.uniform(0, 1, 51)
+
+dfObama = predictwise[mask]
+
+print dfObama.Votes.sum()
+
+
+a = np.array([ 3, 5, 7, 8, 9])
+
+print (a > 7).sum()
