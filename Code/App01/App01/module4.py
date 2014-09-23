722637c5f550346d0b3406b729abf537c42473ab
fahad623 <fahad2191@gmail.com>
2014-09-23 17:30:31 -0500

    a

--- Code/App01/App01/module4.py
+++ Code/App01/App01/module4.py
@@ -1,18 +1,3 @@
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame({'value': np.random.randint(0, 100, 20)})
-
-labels = [ "{0} - {1}".format(i, i + 9) for i in range(0, 100, 10) ]
-
-cuts = pd.cut(df.value, range(0, 105, 10))
-
-bins = np.linspace(0, 1, 20)
-print bins
-print bins[:-1]
-print bins[1:]
-import json
-
 import requests
 import pandas as pd
 import numpy as np
