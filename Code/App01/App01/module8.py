0e507d98febbd8bdb8719f9178e4b25f8d85c778
fahad623 <fahad2191@gmail.com>
2014-09-24 00:23:43 -0500

    a

---
+++ Code/App01/App01/module8.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+text = ['Hop on pop', 'Hop off pop', 'Hop Hop hop']
+print "Original text is\n", '\n'.join(text)
+
+vectorizer = CountVectorizer(min_df=0)
+
+# call `fit` to build the vocabulary
+vectorizer.fit(text)
+
+# call `transform` to convert text to a bag of words
+x = vectorizer.transform(text)
+
+# CountVectorizer uses a sparse array to save memory, but it's easier in this assignment to 
+# convert back to a "normal" numpy array
+x = x.toarray()
+
+print
+print "Transformed text vector is \n", x
+
+# `get_feature_names` tracks which word is associated with each column of the transformed x
+print
+print "Words for each feature:"
+print vectorizer.get_feature_names()
