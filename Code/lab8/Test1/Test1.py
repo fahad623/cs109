3797ffb486bea317c4e18e20dfaf98941e64a394
fahad <fahad2191@gmail.com>
2014-09-29 15:59:58 -0500

    a

---
+++ Code/lab8/Test1/Test1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 29 10:48:00 2014
+
+@author: fahad
+"""
+
+from mrjob.job import MRJob
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, line):
+        yield "chars", len(line)
+        yield "words", len(line.split())
+        yield "lines", 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
