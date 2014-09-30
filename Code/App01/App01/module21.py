3d56834e4de1ab9eee007bb96160bfaacfe14774
fahad623 <fahad2191@gmail.com>
2014-09-30 15:44:19 -0500

    a

---
+++ Code/App01/App01/module21.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, line):
+        #yield "chars", len(line)
+        #yield "words", len(line.split())
+        #yield "lines", 1
+
+        for word in line.split():
+            yield (word, 1)
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
